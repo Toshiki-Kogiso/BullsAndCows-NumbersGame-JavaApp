882f7715643758f8d4a69351c0505d4a704ada00
tkog656 <kogisotoshiki@gmail.com>
2019-09-13 18:10:51 +1200

    optimised

--- src/BullsAndCowsApp/Computer.java
+++ src/BullsAndCowsApp/Computer.java
@@ -1,7 +1,6 @@
 package BullsAndCowsApp;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
 // An class extending player for the computer.
 public abstract class Computer extends Player {
